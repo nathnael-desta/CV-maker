33a03b05da42f99c28d2f5777654594374a35c8c
Nathnael Desta <destanathnaelataro@gmail.com>
2024-10-15 14:11:00 +0300

    add data

---
+++ src/Data.js
@@ -0,0 +1,44 @@
+const data = {
+    general: {
+        firstName: '',
+        lastName: '',
+        title: ''
+    },
+    profile: '',
+    contact: {
+        adress: '',
+        email: '',
+        phoneNo: ''
+    },
+    work: [
+        // {
+        //     companyName: '',
+        //     postion: '',
+        //     details: '',
+        //     start: '',
+        //     end: ''
+        // }
+    ],
+    skills: [],
+    projects: [
+        // {
+        //     title: '',
+        //     details: '',
+        //     start: '',
+        //     end: ''
+        // }
+    ],
+    courses: [
+        // {
+        //     courseName: '',
+        //     offeredBy: '',
+        //     details: '',
+        //     start: '',
+        //     end: ''
+        // }
+    ],
+    interests: [],
+    languages: []
+    
+
+}
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,15 +1,22 @@
 import InputCss from "../styles/Input.module.css";
 import { Images } from "../utils";
+import { useState } from "react";
 
 const Input = ({ icon, iconAlt, title }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setExpanded(prevExpanded => !prevExpanded)
+  }
+
   return (
-    <div className={InputCss.input}>
+    <div className={InputCss.input} onClick={toggleExpanded}>
       <div className={InputCss.left}>
         <img className={`${InputCss.svgs} ${title == 'Profile' ? InputCss.smaller : title == 'Profile' ? InputCss.smaller : null}`} src={icon} alt={iconAlt} />
         <div>{title}</div>
       </div>
       <div className={InputCss.right}>
-        <img src={Images.rightArrow} alt="expand" />
+        <img className={`${InputCss.arrow} ${expanded ? InputCss.rotated : null}`} src={Images.rightArrow} alt="expand" />
       </div>
     </div>
   );
